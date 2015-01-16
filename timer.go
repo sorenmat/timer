196d0cef8614999b5c0d565b447860a98d808fd8
Soren Mathiasen <smo@tradeshift.com>
2015-01-16 15:16:25 +0100

    added godoc

--- timer.go
+++ timer.go
@@ -7,15 +7,19 @@ var (
 	lastRun int64
 )
 
+// TimerStart starts the timer, and keeps that value in a variable
 func TimerStart() {
 	start = time.Now().UnixNano()
 }
 
+// TimerStop stops the timer and registers the time. It returns the difference between start and stop in nano seconds.
+// A call to TimeSpent can be called to retrieve this value later.
 func TimerStop() int64 {
 	lastRun = time.Now().UnixNano()
 	return lastRun
 }
 
+// TimeSpent returns the difference between the call to TimerStart and TimerStop in Nano seconds
 func TimeSpent() int64 {
 	return lastRun
 }
